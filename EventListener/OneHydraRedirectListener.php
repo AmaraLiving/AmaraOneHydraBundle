86bfbeb1d793a3bb234f41fbbd0d5c554e77489d
Vincenzo Trapani <vincenzo.trapani@amara.com>
2015-08-10 15:57:01 +0100

    Fixed wrong var

--- EventListener/OneHydraRedirectListener.php
+++ EventListener/OneHydraRedirectListener.php
@@ -1,9 +1,8 @@
 <?php
 namespace Amara\Bundle\OneHydraBundle\EventListener;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\HttpFoundation\Response;
 use Amara\Bundle\OneHydraBundle\Service\PageManager;
 
 class OneHydraRedirectListener {
@@ -24,12 +23,12 @@ class OneHydraRedirectListener {
 	 * @param GetResponseEvent $event
 	 */
 	public function onKernelRequest(GetResponseEvent $event) {
-		$request = $request->getRequest();
+		$request = $event->getRequest();
 		$pathInfo = $request->getPathInfo();
 
 		if ($oneHydraPage = $this->pageManager->getPage($pathInfo, 'uk')) {
 			$pageObject = $oneHydraPage->getPageObject();
-
+			
 			if (in_array($pageObject->getRedirectCode(), [301, 302])) {
 				$event->setResponse(new RedirectResponse($pageObject->getRedirectUrl(), $pageObject->getRedirectCode()));
 			}
